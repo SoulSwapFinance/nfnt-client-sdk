63b114715a3320402a634998adc47b3a51b3f21c
Diego Ramos <ramosdiego@pm.me>
2022-07-07 14:34:24 -0400

    wip: imports

--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next'
+import Modal from '../../src/components/Modal'
+
+const Trigger = <button>Trigger</button>
 
 const Index: NextPage = () => {
-  return <div></div>
+  return (
+    <Modal trigger={Trigger} title="Title">
+      Content
+    </Modal>
+  )
 }
 
 export default Index
